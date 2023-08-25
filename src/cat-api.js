fe12a04fe79aab688a5b1ba08b28e1f2ebe11b01
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-25 13:14:25 +0200

    fix funct fetchCatByBreed

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 // axios.defaults.headers.common["x-api-key"] = "live_yhNqPNosE5VgQrJCEBVvAw9Au1z9kRoQQbLNilRJQliutJoVguMi2HuJv8UgwvvL";
-
+import Notiflix from 'notiflix';
 
 export function fetchBreeds() {
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
@@ -8,9 +8,13 @@ export function fetchBreeds() {
         
   return fetch(`${BASE_URL}?api_key=${API_KEY}`)
   .then( (resp) =>{
-    if (!resp.ok) {throw new Error(resp.statusText)}
+    if (!resp.ok) {Notiflix.Notify.warning('Oops! Something went wrong! Try reloading the page!')}
     return resp.json();
+    
+
   })
-  .catch(err => console.log(err))
+    .catch(err => {
+      Notiflix.Notify.warning('Oops! Something went wrong! Try reloading the page!');
+    })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {fetchBreeds} from './cat-api'
+import { fetchBreeds } from './cat-api'
+import Notiflix from 'notiflix';
 
 const refs = {
     breedsSelect: document.querySelector(".breed-select"),
@@ -6,60 +7,81 @@ const refs = {
     error: document.querySelector('.error'),
     catInfo: document.querySelector('.cat-info'),
 };
-refs.loader.innerHTML
+refs.loader.classList.add('is-hidden')
 refs.error.classList.add('is-hidden')
 
-fetchBreeds().then((data) => {
+fetchBreeds().then((data) => {  
   data.map(elem => {
-    const qwe = elem.id
     refs.breedsSelect.insertAdjacentHTML('beforeend', `<option value="${elem.id}">${elem.name}</option>`);
-
-
-refs.breedsSelect.addEventListener('change', (e)=> {
-      e.preventDefault();
-      refs.loader.classList.add('is-hidden')
-      
-  if (e.target.value === qwe) {
-      create(elem);
-      
-  } else {
-    refs.error.classList.add("is-hidden")
-    }
-}  )
-
-
   })
 })
-  .catch((err) => {
-    refs.error.classList.add("is-hidden");
-    console.log(err);
-  }).finally(() => {
-    // .classList.remove("is-hidden");
-  })
+  .catch(() => Notiflix.Notify.warning('Oops! Something went wrong! Try reloading the page!')
+)
 
 
-function create(oneEl) {
-  const { name, description, temperament, image: { url } } = oneEl;
+refs.breedsSelect.addEventListener('change', clickEL)
+
+function clickEL(e) {
+    e.preventDefault();
+  fetchCatByBreed(e)
+
+  function fetchCatByBreed(breedId) {
+    const URL = 'https://api.thecatapi.com/v1/images/search';
+    const API_KEY = 'live_yhNqPNosE5VgQrJCEBVvAw9Au1z9kRoQQbLNilRJQliutJoVguMi2HuJv8UgwvvL';
+    const breed_ids = e.target.value;
+    return fetch(`${URL}?api_key=${API_KEY}&breed_ids=${breed_ids}`)
+      .then((resp) => {
+        console.log(resp);
+        if (!resp.ok) { Notiflix.Notify.warning('Oops! Something went wrong! Try reloading the page!') }
+        return resp.json();
+      }).then((data) => {
+        console.log(data);
+        console.log(data[0].breeds[0]);
+        if (e.target.value === data[0].breeds[0].id) {
+          renderCatCard(data);
+        }
+      })
+  
+      .catch(Notiflix.Notify.warning('Oops! Something went wrong! Try reloading the page!')
+      )
+
+  }
+}
+      
+function renderCatCard(json) {
+  const breedInfo = json[0].breeds[0];
+  const img = {
+    url: json[0].url,
+    alt: breedInfo.name,
+  };
   const markup = `
-    
-   <div class="blok-img">
-      <img src="${url}" alt="Cat breed ${name}" class="image" width="500" height="500">
+     <div class="blok-img">
+      <img src="${img.url}" alt="Cat breed ${img.alt}" class="image" width="500" height="500">
    </div>
    <div class="card">
-       <h2 class="header">${name}</h2>
+       <h2 class="header">${breedInfo.name}</h2>
        <div class="description">
-        <p class="text">${description}</p>
-        <p class="text"><b>Temperament:</b> ${temperament}</p>
+        <p class="text">${breedInfo.description}</p>
+        <p class="text"><b>Temperament:</b> ${breedInfo.temperament}</p>
       </div>
    </div>
   `;
   refs.catInfo.innerHTML = markup;
 }
 
-function fetchCatByBreed(breedId){
 
-}
 
+// і тоді в зен передати ту ф - цію.then(renderCatCard)
+
+
+//   if (e.target.value === qwe) {
+//       create(elem);
+      
+//   } else {
+//     refs.error.classList.add("is-hidden")
+    
+// }  
+// }
 
 // fetchBreeds().then((data) => {
 //     console.log(data);
@@ -78,26 +100,3 @@ function fetchCatByBreed(breedId){
 //  )
 // })
     
- 
-
-
-// function renderCatCard(json) {
-//   const breedInfo = json[0].breeds[0];
-//   const img = {
-//     url: json[0].url,
-//     alt: breedInfo.name,
-//   };
-//   const markup = `
-//     <h2 class="header">${breedInfo.name}</h2>
-//     <div class="card">
-//       <img src="${img.url}" alt="Cat breed ${img.alt}" class="image">
-//       <div class="description">
-//         <p class="text">${breedInfo.description}</p>
-//         <p class="text"><b>Temperament:</b> ${breedInfo.temperament}</p>
-//       </div>
-//     </div>
-//   `;
-//   refs.catInfo.innerHTML = markup;
-// }
-// і тоді в зен передати ту ф - цію.then(renderCatCard)
-
